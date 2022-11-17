ca7d0b644483fd134236134004441de9a1e89891
Oliver Finlayson <ofinlayson@lcs.on.ca>
2022-11-17 09:09:03 -0500

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Oliver Finlayson on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
